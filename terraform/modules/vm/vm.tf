5bc962d9c6c85a0c45d75855df3e991ddaf32161
thanhtina8 <thanhtina8@gmail.com>
2024-11-06 23:22:46 +0700

    Update vm.tf

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -24,7 +24,7 @@ resource "azurerm_linux_virtual_machine" "test" {
  
   admin_ssh_key {
     username   = "${var.admin_username}"
-    public_key = "file("~/.ssh/id_rsa.pub")"
+    public_key = file("~/.ssh/id_rsa.pub")
   }
   os_disk {
     caching           = "ReadWrite"
