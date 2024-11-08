0e222ef1343bf8cb10b59180d4ff667cbdc7a66e
thanhtina8 <thanhtina8@gmail.com>
2024-11-08 15:25:45 +0700

    Update appservice.tf

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -3,7 +3,7 @@ resource "azurerm_service_plan" "test" {
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
   os_type             = "Windows"
-  sku_name            = "B1"
+  sku_name            = "A1"
 }
 
 resource "azurerm_windows_web_app" "test" {
