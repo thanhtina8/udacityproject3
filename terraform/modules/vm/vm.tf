64a0ffd5ef9250f379bc081230f7df2d99484632
thanhtina8 <thanhtina8@gmail.com>
2024-11-08 14:17:42 +0700

    Update vm.tf

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -24,7 +24,7 @@ resource "azurerm_linux_virtual_machine" "test" {
  
   admin_ssh_key {
     username   = "${var.admin_username}"
-    public_key = file("/home/vsts/work/_temp/id_rsa.pub")
+    public_key =  "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDIz7/mG5RQusBNJbn2AVKd8XxRaLrTCoUxi3lgIy8u7/XbbID77OG5hkiBLMRiaXW+i+6gPEE4sYkVOvgFV/RbeGiy765mEzP2ZqHsl4JZPFDqUhxjnLwObWHJYp3+W4uebEfjB3w5EJVep/v/DqGIq1Ik1DezaAKBFeZ+mhWSt1uXRyxQQJW43A3cp32z0nNGsJXykSh7eWaOt6NZ6bIOikIFDU7otTzjjPjFCGjLLaQk0qj3ZDQ8ElM2FrPZaB60VjHVmUv+WW6ZceKsrpDge7iNYJdWahoND9e5LOggKlcXQJzrX/nRayZsnC0qzP8fnXppOtrw5bStpFOOP9Koukkeyro8JgfvXOPGMtvOEyP5qmZVjScs9zVwKm5cxeAeH08sWDaDZUgObV4yudXm3yYAXXqrJidpKjlIQ6za9rTADXvuLcqU1XrCDlRzqKgHEDRDtvLCLHp7Bt+y0DfdJx+Nv2b2S7N4OZfVafibbtUQJKzI2u7E8B6/hkY7cPM= odl_user@SandboxHost-638666411326198052"
   }
   os_disk {
     caching           = "ReadWrite"
